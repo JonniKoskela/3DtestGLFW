bfac8bed0722452800dbdb11e511828e50636109
JonniKoskela <Jonni.koskela@gmail.com>
2023-12-07 19:28:22 +0200

    Quaternion + fix

--- 3Dtest/src/main.cpp
+++ 3Dtest/src/main.cpp
@@ -8,7 +8,7 @@
 /**/
 
 
-const char* vertexShaderSource = "#version 330 core\n"
+const char* vertexShaderSource = "#version 330\n"
 "layout(location = 0) in vec3 Position;\n"
 "layout(location = 1) in vec3 vertexColor;\n"
 
@@ -23,61 +23,61 @@ const char* vertexShaderSource = "#version 330 core\n"
 
 
 
-const char* fragmentShaderSource = "#version 330 core\n"
+const char* fragmentShaderSource = "#version 330\n"
 "out vec4 FragColor;\n"
 "in vec3 fragmentColor;\n"
 "out vec3 color;\n"
 "void main()\n"
 "{\n"
 "    color = fragmentColor;\n"
-"}\n\0";
+"}\0";
 
 
-
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
+static float Scale = 0.0f;
+static float angleX = Scale;
+static float angleY = Scale;
 int main(void)
 {
-    float vertices[] = {
+
+    static constexpr GLfloat vertices[] = {
         // front
         0.5f, 0.5f, 0.5f,
         -0.5f, 0.5f, -0.5f,
         -0.5f, 0.5f, 0.5f,
         0.5f, -0.5f, -0.5f,
-        //back
         -0.5f, -0.5f, -0.5f,
-         0.5f,  0.5f, -0.5f,
-         0.5f,  -0.5f, 0.5f,
-        -0.5f,  -0.5f, 0.5f
+        0.5f, 0.5f, -0.5f,
+        0.5f, -0.5f, 0.5f,
+        -0.5f, -0.5f, 0.5f,
     };
-    unsigned int indices[] = {
-        // front
+
+    static constexpr GLuint indices[] = {
         0, 1, 2,
-        2, 3, 0,
-        // right
-        1, 5, 6,
-        6, 2, 1,
-        // back
-        7, 6, 5,
-        5, 4, 7,
-        // left
-        4, 0, 3,
-        3, 7, 4,
-        // bottom
-        4, 5, 1,
-        1, 0, 4,
-        // top
-        3, 2, 6,
-        6, 7, 3
+        1, 3, 4,
+        5, 6, 3,
+        7, 3, 6,
+        2, 4, 7,
+        0, 7, 6,
+        0, 5, 1,
+        1, 5, 3,
+        5, 0, 6,
+        7, 4, 3,
+        2, 1, 4,
+        0, 2, 7
     };
+
     static constexpr GLfloat g_color_buffer_data[] = {
-    0.583f,  0.771f,  0.014f,
-    0.609f,  0.115f,  0.436f,
-    0.327f,  0.483f,  0.844f,
-    0.822f,  0.569f,  0.201f,
-    0.435f,  0.602f,  0.223f,
-    0.310f,  0.747f,  0.185f,
-    0.597f,  0.770f,  0.761f,
-    0.559f,  0.436f,  0.730f,
+    1.0, 0.4, 0.6,
+    1.0, 0.9, 0.2,
+    0.7, 0.3, 0.8,
+    0.5, 0.3, 1.0,
+    0.2, 0.6, 1.0,
+    0.6, 1.0, 0.4,
+    0.6, 0.8, 0.8,
+    0.4, 0.8, 0.8,
     };
+
 
     GLuint colorbuffer;
     GLuint gWorldLocation{};
@@ -102,7 +102,7 @@ int main(void)
 
     glfwMakeContextCurrent(window);
 /*    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);*/
-
+    glfwSetKeyCallback(window, keyCallback);
 
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
@@ -119,23 +119,24 @@ int main(void)
 
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 24, vertices, GL_STATIC_DRAW);
 
 
     glGenBuffers(1, &EBO);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * 36, indices, GL_STATIC_DRAW);
+    std::cout << sizeof(indices);
 
 
     glGenBuffers(1, &colorbuffer);
     glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*24, g_color_buffer_data, GL_STATIC_DRAW);
     glBindVertexArray(0);
 
-    glEnable(GL_CULL_FACE);
+/*    glEnable(GL_CULL_FACE);
     glFrontFace(GL_CW);
     glCullFace(GL_BACK);
-
+*/
 
     //---------------------------------------------------------------------------------------------------------------------------------------------------------------------->
 
@@ -181,50 +182,69 @@ int main(void)
     glDeleteShader(fragmentShader);
 
 
-
-
-
-    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         glUseProgram(shaderProgram);
-        static float Scale = 0.00f;
 
-        Scale += 0.005f;
-        Matrix4f Rotation = {   cosf(Scale), 0.0f,   -sinf(Scale), 0.0f,
+
+        // Calculate half angles for quaternion
+        float halfAngleX = angleX * 0.5f;
+        float halfAngleY = angleY * 0.5f;
+
+        // Quaternion representations of rotations around X and Y axes
+        Quaternionf quatRotationX = Quaternionf(cosf(halfAngleX), sinf(halfAngleX), 0.0f, 0.0f);
+        Quaternionf quatRotationY = Quaternionf(cosf(halfAngleY), 0.0f, sinf(halfAngleY), 0.0f);
+
+        Quaternionf quatRotation = quatRotationX * quatRotationY;
+
+        Matrix4f Rotation = quatRotation.toMatrix();
+        
+        /*Matrix4f RotationY = { cosf(Scale), 0.0f,   -sinf(Scale), 0.0f,
                                 0.0f,        1.0f,   0.0f,         0.0f,
                                 sinf(Scale), 0.0f,   cosf(Scale),  0.0f,
                                 0.0f,        0.0f,   0.0f,         1.0f };
 
+        Matrix4f RotationX = {  1.0f ,        0.0f,   0.0f,           0.0f,
+                                0.0f, cosf(Scale),   sinf(Scale),   0.0f,
+                                0.0f, -sinf(Scale),  cosf(Scale),   0.0f,
+                                0.0f,        0.0f,    0.0f,            1.0f };
+
+        Matrix4f Rotation = RotationX * RotationY;*/
+
         Matrix4f Translation = { 1.0f, 0.0f, 0.0f, 0.0f,
                                  0.0f, 1.0f, 0.0f, 0.0f,
-                                 0.0f, 0.0f, 1.0f, 0.0,
+                                 0.0f, 0.0f, 1.0f, 2.0f,
                                  0.0f, 0.0f, 0.0f, 1.0f };
 
-        float FOV = 90.0f;
+        float FOV = 100.0f;
         float tanHalfFOV = tanf(toRadian(FOV / 2.0f));
         float f = 1 / tanHalfFOV;
 
-        Matrix4f Projection = { 1.0f, 0.0f, 0.0f, 0.0f,
-                                0.0f, 1.0f, 0.0f, 0.0f,
-                                0.0f, 0.0f, 1.0f, 0.0f,
-                                0.0f, 0.0f, 0.0f, 1.0f };
+        Matrix4f Projection = { f, 0.0f, 0.0f, 0.0f,
+                                0.0f, f, 0.0f, 0.0f,
+                                0.0f, 0.0f, 0.0f, 0.0f,
+                                0.0f, 0.0f, 1.0f, 0.0f };
 
         Matrix4f FinalMatrix = Projection * Translation * Rotation;
         glUniformMatrix4fv(gWorldLocation, 1, GL_TRUE, &FinalMatrix.data[0][0]);
 
+
+
         glBindBuffer(GL_ARRAY_BUFFER, VBO);
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),0 );
 
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(GLfloat), 0);
+
+        glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
         glEnableVertexAttribArray(1);
-        glVertexAttribPointer( 1, 3, GL_FLOAT, GL_FALSE,          0,       0 );
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
 
         glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
@@ -240,17 +260,45 @@ int main(void)
         glfwPollEvents();
 
     }
+    glDeleteProgram(shaderProgram);
     glDeleteBuffers(1, &VBO);
     glDeleteBuffers(1, &EBO);
-    glDeleteProgram(shaderProgram);
-
 
 
     glfwTerminate();
     return 0;
 }
 
-
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    std::cout << key << "\n";
+    switch(key){
+    case GLFW_KEY_A:
+    {
+        angleY += 0.07f;
+        break;
+    }
+    case GLFW_KEY_D:
+    {
+        angleY -= 0.07f;
+        break;
+    }
+    case GLFW_KEY_W:
+    {
+        angleX += 0.07f;
+        break;
+    }
+    case GLFW_KEY_S:
+    {
+        angleX -= 0.07f;
+        break;
+    }
+    default:
+    {
+        break;
+    }
+    }
+    
+}
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
--- 3Dtest/src/utils.h
+++ 3Dtest/src/utils.h
@@ -48,3 +48,48 @@ struct Vec2 {
 		}
 	};
 
+	class Quaternionf {
+	private:
+		float w, x, y, z;
+
+	public:
+		// Constructor
+		Quaternionf(float qw, float qx, float qy, float qz) : w(qw), x(qx), y(qy), z(qz) {}
+
+		// Method to convert the quaternion to a rotation matrix
+		Matrix4f toMatrix() const {
+			Matrix4f rotationMatrix;
+
+			// Assuming Matrix4f is a 4x4 matrix type
+			rotationMatrix.data[0][0] = 1 - 2 * (y * y + z * z);
+			rotationMatrix.data[0][1] = 2 * (x * y - w * z);
+			rotationMatrix.data[0][2] = 2 * (x * z + w * y);
+			rotationMatrix.data[0][3] = 0.0f;
+
+			rotationMatrix.data[1][0] = 2 * (x * y + w * z);
+			rotationMatrix.data[1][1] = 1 - 2 * (x * x + z * z);
+			rotationMatrix.data[1][2] = 2 * (y * z - w * x);
+			rotationMatrix.data[1][3] = 0.0f;
+
+			rotationMatrix.data[2][0] = 2 * (x * z - w * y);
+			rotationMatrix.data[2][1] = 2 * (y * z + w * x);
+			rotationMatrix.data[2][2] = 1 - 2 * (x * x + y * y);
+			rotationMatrix.data[2][3] = 0.0f;
+
+			rotationMatrix.data[3][0] = 0.0f;
+			rotationMatrix.data[3][1] = 0.0f;
+			rotationMatrix.data[3][2] = 0.0f;
+			rotationMatrix.data[3][3] = 1.0f;
+
+			return rotationMatrix;
+		}
+
+		Quaternionf operator*(const Quaternionf& other) const {
+			float newW = w * other.w - x * other.x - y * other.y - z * other.z;
+			float newX = w * other.x + x * other.w + y * other.z - z * other.y;
+			float newY = w * other.y - x * other.z + y * other.w + z * other.x;
+			float newZ = w * other.z + x * other.y - y * other.x + z * other.w;
+
+			return Quaternionf(newW, newX, newY, newZ);
+		}
+	};
